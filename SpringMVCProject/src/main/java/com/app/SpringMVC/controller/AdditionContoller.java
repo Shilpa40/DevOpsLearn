7e8c989c401c2a0220a52879b215b8494beded24
Shilpa <64268141+Shilpa40@users.noreply.github.com>
2022-03-27 17:21:13 +0530

    Java WebApp

---
+++ SpringMVCProject/src/main/java/com/app/SpringMVC/controller/AdditionContoller.java
@@ -0,0 +1,26 @@
+package com.app.SpringMVC.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class AdditionContoller {
+	@RequestMapping("/add")
+	public ModelAndView getAdditionResult(HttpServletRequest request, HttpServletResponse response ) {
+		ModelAndView mv = new ModelAndView();
+		
+		int a = Integer.parseInt(request.getParameter("t1"));
+		int b = Integer.parseInt(request.getParameter("t2"));
+		
+		int c = a+b;
+		mv.addObject("result", c);
+		mv.setViewName("add-result");
+		return mv;
+		
+	}
+}
